d6960d5a02f21d42a2d66d8e87a4b94e4b7567db
ETTSU-StarTail <holywolf53@gmail.com>
2023-09-04 11:06:04 +0900

    feat: 頻度に応じたチェックのためループインターバルを実装 feat: 記録のためのロギングを実装

--- AutoReconnect.ps1
+++ AutoReconnect.ps1
@@ -16,12 +16,18 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 Write-Host "無限ループのため、Ctrl + C で終了してください。"
 
 do {
+    $DateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $AdapterStatus = (Get-NetAdapter "$AdapterName").Status
-    Write-Host "$AdapterName の接続状態: $AdapterStatus"
+    $AdapterStatusLog = "$DateTime - $AdapterName の接続状態: $AdapterStatus"
+
+    Write-Host $AdapterStatusLog
+    Write-Output $AdapterStatusLog | Out-File -FilePath reconnect.log -Encoding utf8 -Append
+
     if ($AdapterStatus -eq "Disconnected") {
-        Write-Host "$AdapterName の切断を検知、再起動します。"
+        Write-Output "$AdapterName の切断を検知 → 再起動" | Out-File -FilePath reconnect.log -Encoding utf8 -Append
         Restart-NetAdapter $AdapterName
     }
 
-    cmd /c timeout $Interval
+    # cmd /c timeout $Interval
+    Start-Sleep -Seconds $Interval
 } while ($true)
